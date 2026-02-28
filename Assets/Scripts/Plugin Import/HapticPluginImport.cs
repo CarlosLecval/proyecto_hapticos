02140cd32b0ba48c6165869ffe568a92a2e6b9bf
CarlosLecval <carloslecval03@gmail.com>
2026-02-27 20:05:41 -0600

    Change dll depending of os

--- Assets/Scripts/Plugin Import/HapticPluginImport.cs
+++ Assets/Scripts/Plugin Import/HapticPluginImport.cs
@@ -6,54 +6,62 @@ using UnityEngine;
 
 public class HapticPluginImport : MonoBehaviour {
 
+#if UNITY_STANDALONE_WIN
+    private const string HapticDLL = "C++ DLL for Unity";
+#elif UNITY_STANDALONE_OSX
+    private const string HapticDLL = "libGodot_Haptic_Plugin";
+#else
+    private const string HapticDLL = "libGodot_Haptic_Plugin"; // Default to macOS
+#endif
+
     // Constructor
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern IntPtr CreateHapticDevices();
 
     // Destructor
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern void DeleteHapticDevices(IntPtr hapticPlugin);
 
     // get haptic device information
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern int GetHapticsDetected(IntPtr hapticPlugin);
 
     // get haptic device information
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern double GetHapticsDeviceInfo(IntPtr hapticPlugin, int numHapDev, int var);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern Vector3 GetHapticsPositions(IntPtr hapticPlugin, int numHapDev);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern Quaternion GetHapticsOrientations(IntPtr hapticPlugin, int numHapDev);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern double GetHapticsGripperAngle(IntPtr hapticPlugin, int numHapDev);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern Vector3 GetHapticsLinearVelocity(IntPtr hapticPlugin, int numHapDev);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern Vector3 GetHapticsAngularVelocity(IntPtr hapticPlugin, int numHapDev);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern double GetHapticsGripperAngularVelocity(IntPtr hapticPlugin, int numHapDev);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern bool GetHapticsButtons(IntPtr hapticPlugin, int numHapDev, int button);
 
     // set haptic device forcefeedback
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern void SetHapticsForce(IntPtr hapticPlugin, int numHapDev, Vector3 sentForce);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern void SetHapticsTorque(IntPtr hapticPlugin, int numHapDev, Vector3 sentTorque);
 
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern void SetHapticsGripperForce(IntPtr hapticPlugin, int numHapDev, double sentGripperForce);
 
     // main haptics rendering loop
-    [DllImport("libGodot_Haptic_Plugin")]
+    [DllImport(HapticDLL)]
     public static extern void UpdateHapticDevices(IntPtr hapticPlugin, int numHapDev);
 }
